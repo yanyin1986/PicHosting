f70c4b3009565d814b513e908bc8e199c5dc5c7e
Leon.yan <yanyin1986@gmail.com>
2018-02-21 07:32:14 +0900

    First Commit

---
+++ PicHosting/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PicHosting
+//
+//  Created by Leon.yan on 2018/2/21.
+//  Copyright © 2018 Leon.yan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PicHosting/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PicHosting
+//
+//  Created by Leon.yan on 2018/2/21.
+//  Copyright © 2018 Leon.yan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
